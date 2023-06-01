f7bb7d41642cace3fe94698b0b5566c60e4ee904
XimenaSanchezUwU <126804482+XimenaSanchezUwU@users.noreply.github.com>
2023-06-01 08:29:47 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
 
 let img;
+let myFont;
 function preload() {
-  img = loadImage('Images/Modi.png');
+  img = loadImage('assets/Modi.png');
+  myFont = loadFont('assets/CherryHand-Regular.ttf');
 }
 
 //Hacer la imagen menor a 1000 pixeles
+//Cargar la fuente loadFont()-->
+
+/*
+let myFont;
+function preload() {
+  myFont = loadFont('assets/Handmade.otf');
+}
+*/
 
 
 function setup() {
   createCanvas(511,784);
+  textFont(myFont);
 }
 
 function draw() {
   background(220);
-  image(img, 0, 0, 511, 784);
+  image(img, 0, 0, 1511, 1784);
   text('¿Hacia donde voy con todo esto?', 33, 411);
-  textFont('Bradley Hand ITC');
-  textSize(30);
+  //textFont('myFont');
   fill(random(122), 132, 123);
   text('¿Hacia donde voy con todo esto?', random(100), 111);
-  textFont('Bradley Hand ITC');
+  //textFont('myFont');
   textSize(random(40));
   fill(11, 32, 123);
   text('¿Hacia donde voy con todo esto?', 20, random(631));
-  textFont('Bradley Hand ITC');
+  //textFont('myFont');
   textSize(35);
   fill(222,1,12);
 }
